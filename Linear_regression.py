1c837502d28f7395837b3c3a0fee6b7bbbbcb675
Cnguyen219 <158215758+Cnguyen219@users.noreply.github.com>
2026-02-23 15:01:11 -0800

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -2,4 +2,13 @@ import numpy as np
 
 class LinearRegression:
   def __init__(self, learning_rate=0.01, iterations=1000):
-  
+        self.learning_rate = learning_rate
+        self.iterations = iterations
+        self.w = 0
+        self.b = 0
+  def compute_cost(self, X, y):
+        m = len(X)
+        predictions = self.w * X + self.b
+        cost = (1 / (2 * m)) * np.sum((predictions - y) ** 2)
+        return cost
+
