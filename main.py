798ebc7d484fc0e71d92d23fc31400e7861d569a
Cnguyen219 <158215758+Cnguyen219@users.noreply.github.com>
2026-02-23 21:28:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import numpy as np
-
 from linear_regression import LinearRegression
 
+# Example data (house size vs price)
+X = np.array([500, 800, 1200, 1500, 2000])
+y = np.array([150000, 220000, 300000, 350000, 450000])
+
+model = LinearRegression(learning_rate=0.00000001, iterations=1000)
+model.fit(X, y)
+
+prediction = model.predict(np.array([1800]))
+
+print("Weight (w):", model.w)
+print("Bias (b):", model.b)
+print("Prediction for 1800 sq ft:", prediction[0])
