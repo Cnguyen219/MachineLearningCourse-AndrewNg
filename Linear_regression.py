cd1c100e028ab81dcfd7f3a578781c65036cb72e
Cnguyen219 <158215758+Cnguyen219@users.noreply.github.com>
2026-02-23 21:28:07 -0800

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -12,3 +12,18 @@ class LinearRegression:
         cost = (1 / (2 * m)) * np.sum((predictions - y) ** 2)
         return cost
 
+ def fit(self, X, y):
+        m = len(X)
+
+        for _ in range(self.iterations):
+            predictions = self.w * X + self.b
+
+            dw = (1 / m) * np.sum((predictions - y) * X)
+            db = (1 / m) * np.sum(predictions - y)
+
+            self.w -= self.learning_rate * dw
+            self.b -= self.learning_rate * db
+
+    def predict(self, X):
+        return self.w * X + self.b
+
